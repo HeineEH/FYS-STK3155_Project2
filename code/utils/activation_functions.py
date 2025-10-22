a9488f157fa2a5a21c0f2c8bdecd95ec5609baa3
Heine Husdal <heine.elias.husdal@gmail.com>
2025-10-22 13:24:23 +0200

    Added activation functions

---
+++ code/utils/activation_functions.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from typing import Any
+import numpy
+import autograd.numpy as np # type: ignore
+np: numpy = np # type: ignore . Workaround to not get type errors when using autograd's numpy wrapper.
+
+
+class _ActivationFunction(ABC):
+    @abstractmethod
+    def __call__(self, z):
+        pass
+
+    @abstractmethod
+    def derivative(self, z) -> Any:
+        pass
+
+
+class Sigmoid(_ActivationFunction):
+    def __call__(self, z):
+        return 1 / (1 + np.exp(-z))
+    
+    def derivative(self, z):
+        sig = self.__call__(z)
+        return sig * (1 - sig)
+
+class Identity(_ActivationFunction):
+    def __call__(self, z):
+        return z
+    
+    def derivative(self, z):
+        return 1
+
+class ReLU(_ActivationFunction):
+    def __call__(self, z):
+        return np.maximum(0, z)
+
+    def derivative(self, z):
+        return (np.where(z > 0, 1, 0))
+    
+
+class Softmax(_ActivationFunction):
+    def __call__(self, z):
+        z_shift = z - np.max(z, axis=1, keepdims=True)  # To avoid taking exp of too large numbers. Gives same result.
+        exp_z = np.exp(z_shift) 
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+    def derivative(self, z):
+        raise NotImplementedError("Derivative of Softmax is not implemented. Use `SoftmaxCrossEntropy` cost function instead, with no last layer activation (identity func).")
