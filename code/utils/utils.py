c7ae4469475c9c0c878008f48298fb4b4e19a28e
Heine Husdal <heine.elias.husdal@gmail.com>
2025-10-23 14:15:26 +0200

    Added changeable noise to generate_dataset

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -10,8 +10,8 @@ T = TypeVar("T", float, NDArray[numpy.floating])
 def runge(x: T) -> T:
     return 1/(1 + 25*x**2)
 
-def generate_dataset(num: int = 400):
+def generate_dataset(num = 400, noise = 0.05):
     x = np.linspace(-3, 3, num).reshape(-1, 1)
     np.random.shuffle(x)
-    y = runge(x) + 0.05*np.random.normal(0, 1, (num, 1))
+    y = runge(x) + noise*np.random.normal(0, 1, (num, 1))
     return x, y
