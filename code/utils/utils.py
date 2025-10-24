5c5ffa121f02f4f3d1f34e1d58fec2ec55ce014f
Heine Husdal <heine.elias.husdal@gmail.com>
2025-10-25 00:45:19 +0200

    Added util function to check that layer params are equal

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 # Typing
-from .typing_utils import ArrayF
+from .typing_utils import ArrayF, NetworkParams
 from typing import TYPE_CHECKING, TypeVar
 if TYPE_CHECKING:
     import numpy as np  # typed NumPy for the checker
@@ -18,3 +18,16 @@ def generate_dataset(num = 400, noise = 0.05):
     np.random.shuffle(x)
     y = runge(x) + noise*np.random.normal(0, 1, (num, 1))
     return x, y
+
+def check_layer_params_equality(params1: NetworkParams, params2: NetworkParams) -> bool:
+    if len(params1) != len(params2):
+        raise ValueError("params1 and params2 must have the same number of layers")
+    
+    for (p1_W, p1_b), (p2_W, p2_b) in zip(params1, params2):
+        if p1_W.shape != p2_W.shape or p1_b.shape != p2_b.shape:
+            raise ValueError("Layer parameter shapes do not match")
+
+        if not (np.allclose(p1_W, p2_W) and np.allclose(p1_b, p2_b)):
+            return False
+        
+    return True
