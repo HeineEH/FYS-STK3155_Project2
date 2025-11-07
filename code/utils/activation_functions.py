29d34f2c04908cc86de240f93b8b20b0c3e10884
Heine Husdal <heine.elias.husdal@gmail.com>
2025-11-07 21:42:23 +0100

    Added more typing and docstrings

--- code/utils/activation_functions.py
+++ code/utils/activation_functions.py
@@ -11,51 +11,94 @@ else:
 
 
 class ActivationFunction(ABC):
+    """Abstract base class for activation functions."""
     @abstractmethod
-    def __call__(self, z: ArrayF) -> ArrayF: ...
+    def __call__(self, z: ArrayF) -> ArrayF:
+        """
+        Evaluate the activation function.
+        Parameters:
+            z (NDArray[Float]): Input array.
+        Returns:
+            NDArray[Float]: Activated output array.
+        """
+        ...
 
     @abstractmethod
-    def derivative(self, z: ArrayF) -> ArrayF: ...
+    def derivative(self, z: ArrayF) -> ArrayF:
+        """
+        Evaluate the derivative of the activation function with respect to the input.
+        Parameters:
+            z (NDArray[Float]): Input array.
+        Returns:
+            NDArray[Float]: Derivative of the activation function.
+        """
+        ...
 
 
 class Sigmoid(ActivationFunction):
-    def __call__(self, z):
+    """
+    Sigmoid activation function.
+    f(z) = 1 / (1 + exp(-z))
+
+    Used both in hidden layers, and output layer for binary classification.
+    """
+    def __call__(self, z: ArrayF):
         return 1 / (1 + np.exp(-z))
     
-    def derivative(self, z):
+    def derivative(self, z: ArrayF):
         sig = self.__call__(z)
         return sig * (1 - sig)
     
 class Identity(ActivationFunction):
-    def __call__(self, z):
+    """
+    Identity activation function.
+    f(z) = z
+    """
+    def __call__(self, z: ArrayF):
         return z
     
-    def derivative(self, z):
+    def derivative(self, z: ArrayF):
         return np.ones_like(z)
 
 class ReLU(ActivationFunction):
-    def __call__(self, z):
+    """
+    ReLU activation function.
+    f(z) = max(0, z)
+    """
+    def __call__(self, z: ArrayF):
         return np.maximum(0, z)
 
-    def derivative(self, z):
+    def derivative(self, z: ArrayF):
         return (np.where(z > 0, 1, 0))
     
 class LeakyReLU(ActivationFunction):
-    def __init__(self, negative_slope=0.01):
+    """
+    Leaky ReLU activation function.
+    f(z) = max(0, z) + negative_slope * min(0, z)
+    """
+    def __init__(self, negative_slope: float = 0.01):
         self.negative_slope = negative_slope
     
-    def __call__(self, z):
+    def __call__(self, z: ArrayF):
         return np.maximum(0, z) + self.negative_slope*np.minimum(0, z)
 
-    def derivative(self, z):
+    def derivative(self, z: ArrayF):
         return (np.where(z > 0, 1, self.negative_slope))
 
 class Softmax(ActivationFunction):
-    def __call__(self, z):
+    """
+    Softmax activation function.
+    f(z_i) = exp(z_i) / sum_j[exp(z_j)]
+
+    Typically used in the output layer for multi-class classification.
+    
+    Note: The derivative is not implemented here due to its complexity. However, backpropagation will still work when used with `MulticlassCrossEntropy`.
+    """
+    def __call__(self, z: ArrayF):
         z_shift = z - np.max(z, axis=1, keepdims=True)  # To avoid taking exp of too large numbers. Gives same result.
         exp_z = np.exp(z_shift)
         s: ArrayF = exp_z / np.sum(exp_z, axis=1, keepdims=True)
         return s
 
-    def derivative(self, z): # type: ignore
+    def derivative(self, z: ArrayF): # type: ignore
         raise NotImplementedError("Derivative of Softmax is not implemented.")
